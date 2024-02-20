7398c62a24c33831579deae5988311c20900f710
Marcus-devfs <marcusvini6277@gmail.com>
2024-02-20 11:50:00 -0300

    ajustar cadastro

--- src/atoms/TextInput.js
+++ src/atoms/TextInput.js
@@ -2,21 +2,38 @@ import { TextField } from "@mui/material";
 import { Colors } from "../organisms";
 import { useAppContext } from "../context/AppContext";
 import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
 import { Box } from "./Box";
 import { Text } from "./Text";
+import { useState } from "react";
 
 
 export const TextInput = (props) => {
    const { InputProps = {}, label = '', bold = false, InputLabelProps = {}, small = false } = props;
    const { colorPalette } = useAppContext();
+   const [showPassword, setShowPassword] = useState(false);
+
+   const handleClickShowPassword = () => {
+      setShowPassword((show) => {
+         return !show;
+      });
+   };
+
+   const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+   };
 
    return (
       <TextField
+         {...props}
          autoComplete='off'
          id="fieldssss"
          label={label}
-         {...props}
+         type={props.type === 'password' ? (showPassword ? 'text' : 'password') : props.type}
          InputProps={{
+            ...InputProps,
             autocomplete: 'no',
             autoComplete: 'off',
             form: {
@@ -32,6 +49,19 @@ export const TextInput = (props) => {
                color: colorPalette.textColor,
                maxHeight: props.multiline ? 'none' : '45px',
             },
+
+            endAdornment: props.type === 'password' ? (
+               <InputAdornment position="end">
+                  <IconButton
+                     aria-label="toggle password visibility"
+                     onClick={handleClickShowPassword}
+                     onMouseDown={handleMouseDownPassword}
+                     edge="end"
+                  >
+                     {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+               </InputAdornment>
+            ) : null,
 
             startAdornment: props.type === "coin"
                ? (
