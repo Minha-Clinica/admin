6136e26ca0dcd771603847ddb6827dfcfa76655e
Marcus-devfs <marcusvini6277@gmail.com>
2024-10-09 12:33:48 -0300

    alter public repo

--- src/pages/privacidade/index.js
+++ src/pages/privacidade/index.js
@@ -11,6 +11,7 @@ function Privacity() {
         document.title = `Afectu - Política de Privacidade`;
         return () => window.removeEventListener("resize", () => { });
     }, []);
+    
 
     return (
         <>
